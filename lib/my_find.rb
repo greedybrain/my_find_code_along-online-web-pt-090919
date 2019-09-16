3d5c235cf9fec46d020490858fd902df73301746
sensible-gateway-6049 <greedybrain18@gmail.com>
2019-09-16 12:17:38 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -3,10 +3,8 @@ require 'pry'
 def my_find(collection)
   
   i = 0 
-  while i < collection.length 
-    if yield(collection[i])
-      return collection[i] if yield(collection[i])
-    end
+  while i < collection.length
+    return collection[i] if yield(collection[i])
     i += 1 
   end
   
